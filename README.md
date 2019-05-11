0659faca38b7c33cdd8539731e22752d47b3a8e6
Ivan Aranda <IvanAranda@iMac.local>
2019-05-11 15:52:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpellCheckerTreeSet
---
+++ src/com/inc108/SpellCheckerTreeSet.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.inc108;
+
+/**
+ * @author IvanAranda
+ *	For this lab, you will need to use some of the methods that are defined in the Set interface. 
+ * 	Recall that if set is a Set, then the following methods are defined:
+
+	set.size() -- Returns the number of items in the set.
+	set.add(item) -- Adds the item to the set, if it is not already there.
+	set.contains(item) -- Check whether the set contains the item.
+	set.isEmpty() -- Check whether the set is empty.
+	You will also need to be able to traverse a set, using either an iterator or a for-each loop.
+ */
+public class SpellCheckerTreeSet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
